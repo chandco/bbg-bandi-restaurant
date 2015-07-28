f2728d65257c2c57b361374d6079bf571c79326e
nathanedwards <nathan@dev.chand.co>
2015-07-28 14:42:06 +0100

    First commit

---
+++ extra-footer.php
@@ -0,0 +1,3 @@
+<script>
+	var child_script = "<?php echo get_stylesheet_directory_uri(); ?>/library/js/main.js";
+</script>
---
+++ extra-header.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+?>
+
+
+<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/cd7fcf81-2c45-419e-a674-ad7ffb7cc5ee.css"/>
+
